428c307e1301ee5688ed73144b39f847d8dffbf0
Paul <84803938+PablloZz@users.noreply.github.com>
2024-06-24 09:23:38 +0300

    feat: Implement query to get usdc/weth pair

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,11 @@
-import { QueryParameter, DuneClient, type RunQueryArgs } from "@duneanalytics/client-sdk";
+import { DuneClient } from "@duneanalytics/client-sdk";
 import "dotenv/config";
 
 const { DUNE_API_KEY = "" } = process.env;
 const client = new DuneClient(DUNE_API_KEY);
-const queryId = 1215383;
-const options: RunQueryArgs = {
-  queryId,
-  query_parameters: [
-    QueryParameter.text("TextField", "Plain Text"),
-    QueryParameter.number("NumberField", 3.1415926535),
-    QueryParameter.date("DateField", "2022-05-04 00:00:00"),
-    QueryParameter.enum("ListField", "Option 1"),
-  ],
-};
+const UNISWAP_USDC_WETH_PAIRS_QUERY_ID = 3851868;
 
-client.runQuery(options).then((executionResult) => console.log(executionResult.result?.rows));
+client
+  .getLatestResult({ queryId: UNISWAP_USDC_WETH_PAIRS_QUERY_ID })
+  .then((uniswapUsdcWethPairs) => console.log(uniswapUsdcWethPairs.result?.rows))
+  .catch((error) => console.log(error));
