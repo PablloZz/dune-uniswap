d6f18216bc1a3cab6658be0ebe80ab1eedbbf34d
Pavlo Zalutskiy <kyrogane1@gmail.com>
2024-06-25 19:30:57 +0300

    feat: Get usdc/weth swaps count and total volume

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import { DuneClient } from "@duneanalytics/client-sdk";
 import "dotenv/config";
 import { Config } from "./libs/config";
-import { getUniswapUsdcWethCreatedPair } from "./libs/getQueryResult";
+import {
+  getUniswapUsdcWethCreatedPair,
+  getUniswapUsdcWethSwapsCountAndTotalVolume,
+} from "./libs/getQueryResult";
 
 const client = new DuneClient(Config.ENV.DUNE_API_KEY);
 
 async function init() {
   console.log(await getUniswapUsdcWethCreatedPair(client));
+  console.log(await getUniswapUsdcWethSwapsCountAndTotalVolume(client));
 }
 
 init();
--- src/libs/getQueryResult.ts
+++ src/libs/getQueryResult.ts
@@ -2,7 +2,9 @@ import { type DuneClient, type ResultsResponse } from "@duneanalytics/client-sdk
 import { parseResponse } from "./helpers";
 import { Config } from "./config";
 
+const DUNE_API_EXECUTION_ENDPOINT = "https://api.dune.com/api/v1/execution/";
 const UNISWAP_USDC_WETH_PAIRS_QUERY_ID = 3851868;
+const UNISWAP_USDC_WETH_SWAPS_COUNT_AND_VOLUME_QUERY_ID = 3856794;
 const PING_FREQUENCY = 5;
 
 async function getUniswapUsdcWethCreatedPair(duneClient: DuneClient) {
@@ -13,7 +15,7 @@ async function getUniswapUsdcWethCreatedPair(duneClient: DuneClient) {
   );
 
   const response = await fetch(
-    `https://api.dune.com/api/v1/execution/${uniswapUsdcWethPairsResultStatus.execution_id}/results`,
+    `${DUNE_API_EXECUTION_ENDPOINT}${uniswapUsdcWethPairsResultStatus.execution_id}/results`,
     {
       method: "get",
       headers: {
@@ -24,7 +26,29 @@ async function getUniswapUsdcWethCreatedPair(duneClient: DuneClient) {
 
   const { result } = await parseResponse<ResultsResponse>(response);
 
-  return result;
+  return result?.rows;
 }
 
-export { getUniswapUsdcWethCreatedPair };
+async function getUniswapUsdcWethSwapsCountAndTotalVolume(duneClient: DuneClient) {
+  const uniswapUsdcWethSwapsCountAndTotalVolume = await duneClient.refreshResults(
+    UNISWAP_USDC_WETH_SWAPS_COUNT_AND_VOLUME_QUERY_ID,
+    {},
+    PING_FREQUENCY,
+  );
+
+  const response = await fetch(
+    `${DUNE_API_EXECUTION_ENDPOINT}${uniswapUsdcWethSwapsCountAndTotalVolume.execution_id}/results`,
+    {
+      method: "get",
+      headers: {
+        "X-DUNE-API-KEY": Config.ENV.DUNE_API_KEY,
+      },
+    },
+  );
+
+  const { result } = await parseResponse<ResultsResponse>(response);
+
+  return result?.rows;
+}
+
+export { getUniswapUsdcWethCreatedPair, getUniswapUsdcWethSwapsCountAndTotalVolume };
